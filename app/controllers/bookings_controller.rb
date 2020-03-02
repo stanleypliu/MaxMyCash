62d8f1be2e4b85ce7e2c219191720d48dd1fc0b6
Stanley Liu <sliu1993@hotmail.co.uk>
2020-03-02 18:01:45 +0000

    done some prelim work on listings and bookings controller

---
+++ app/controllers/bookings_controller.rb
@@ -0,0 +1,24 @@
+class BookingsController < ApplicationController
+  before_action :set_listing
+
+  def index
+    @bookings = Booking.where(user: params[:user_id])
+  end
+
+  def show
+    @booking = Booking.find(params[:id])
+  end
+
+  # def new
+  # end
+
+  def create
+  end
+
+  private
+
+  def set_listing
+    @listing = Listing.find(params[:listing_id])
+  end
+
+end
