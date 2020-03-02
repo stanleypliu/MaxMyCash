b33f55de2fb805bb4ac19dc0206257170943e029
Stanley Liu <sliu1993@hotmail.co.uk>
2020-03-02 16:42:45 +0000

    routes created lovely people

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :listings do
+    resources :bookings, only: [:index, :show, :new, :create]
+  end
+  resources :reviews
+  get "dashboard", to: "users#dashboard", as: :dashboard # custom route - we have to create a users controller as well
 end
