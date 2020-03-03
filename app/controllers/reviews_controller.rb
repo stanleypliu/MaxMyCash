350d4a7d77e7be1f29f942682c98cc0ef3598189
Hamza Ahmad <hamza_0786@hotmail.co.uk>
2020-03-03 12:56:03 +0000

    done

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,13 +1,22 @@
 class ReviewsController < ApplicationController
-  def index
-  end
 
-  def new
-  end
+
 
   def create
+    raise
+    # @review = Review.new(review_params)
+    # @review.user = current_user
+    # if @review.save
+    #   redirect_to review_path(@review)
+    # else
+    #   render :new
+    # end
   end
 
-  def destroy
-  end
+  # def destroy
+  #   @review = Review.find(params[:id])
+  #   @review.destroy
+
+  #   redirect_to review_path(@review)
+  # end
 end
