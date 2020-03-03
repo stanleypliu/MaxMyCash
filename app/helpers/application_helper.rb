144dda5833bd212c73cc9f941381176164651135
Stanley Liu <sliu1993@hotmail.co.uk>
2020-03-03 12:04:54 +0000

    basic index and show page created for listings

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def card_link_to(href)
+    "<a href='#{href}'>
+      <div>
+        <div class='listing-card-title'><p><%= listing.location %></p><div class='listing-card-title-money'><p><p><%= listing.type%><%= listing.currency_amount %></p></div></div>
+        <p><%= listing.user.first_name  %> <%= listing.user.last_name %></p>
+        <p><%= listing.message %></p>
+      </div>
+    </a>".html_safe
+  end
 end
