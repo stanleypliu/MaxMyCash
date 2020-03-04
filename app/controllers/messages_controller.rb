e5581f87855c0ffca371345bd630791e877a2df6
danielle levy <danielle.levyy@gmail.com>
2020-03-04 15:12:32 +0000

    almost finished with the chat function

---
+++ app/controllers/messages_controller.rb
@@ -0,0 +1,25 @@
+class MessagesController < ApplicationController
+   def create
+    # raisex
+    @chatroom = Chatroom.find(params[:chatroom_id])
+    @message = Message.new(message_params)
+    @message.chatroom = @chatroom
+    @message.user = current_user
+    if @message.save
+      # send the message out to Action Cable channel for everyone to see
+      ChatroomChannel.broadcast_to(
+        @chatroom,
+        render_to_string(partial: "message", locals: { message: @message })
+      )
+      redirect_to chatroom_path(@chatroom)
+    else
+      render "chatrooms/show"
+    end
+  end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:content)
+  end
+end
