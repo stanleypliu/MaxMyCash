84185628c86db77dfe9a0ff7e65190ed52b4e7a9
Maxime Seiter <maxycle@hotmail.com>
2020-03-05 14:10:25 +0000

    search bar

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -10,6 +10,13 @@ class Listing < ApplicationRecord
   validates :location, presence: true
   validates :message, length: { minimum: 20 }
 
+  include PgSearch::Model
+  pg_search_scope :search_by_currency,
+    against: [ :currency ],
+    using: {
+      tsearch: { prefix: true } # <-- now `superman batm` will return something!
+    }
+
   def self.get_currencies
     # url = 'https://openexchangerates.org/api/currencies.json'
     url = 'https://gist.githubusercontent.com/Fluidbyte/2973986/raw/8bb35718d0c90fdacb388961c98b8d56abc392c9/Common-Currency.json'
@@ -19,10 +26,9 @@ class Listing < ApplicationRecord
 
     parsed_currencies = currencies.to_a.map do |currency|
       currency = "#{currency[1]["symbol"]} - #{currency[1]["name"]}"
-    end
   end
 end
-
+end
 
 
 
