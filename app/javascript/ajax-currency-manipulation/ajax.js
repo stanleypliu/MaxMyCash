50780f4272352cad279b62ea2727443dce5c34ff
Maxime Seiter <maxycle@hotmail.com>
2020-03-06 12:35:49 +0000

    map on the app

--- app/javascript/ajax-currency-manipulation/ajax.js
+++ app/javascript/ajax-currency-manipulation/ajax.js
@@ -2,13 +2,15 @@ const exchangedCurrency = document.querySelector('.exchanged-currency');
 const dropdownInput = document.querySelector('.dropdown-currency');
 const ratesURL = `http://data.fixer.io/api/latest?access_key=6be59aaac4ccb088b8d91e92e87bf7fa`;
 
-dropdownInput.addEventListener('input', (event) => {
-  const currency = event.target.value;
-  const currencyAmount = document.querySelector('.currency-amount').value;
-  const dropdownInputSplit = currency.split(' ')[0];
-  fetch(ratesURL).then(response => response.json()).then((data) => {
-    document.querySelector('.exchanged-currency').innerHTML = "";
-    const exchanged_amount = (currencyAmount / data.rates[dropdownInputSplit]).toFixed(2);
-    exchangedCurrency.insertAdjacentHTML('afterbegin', `<br><div class="currency-span"><h3>You'll get <b>${exchanged_amount} Euros</b> back<h3>!</div></br>`);
-  })
-})
+if (dropdownInput) {
+	dropdownInput.addEventListener('input', (event) => {
+	  const currency = event.target.value;
+	  const currencyAmount = document.querySelector('.currency-amount').value;
+	  const dropdownInputSplit = currency.split(' ')[0];
+	  fetch(ratesURL).then(response => response.json()).then((data) => {
+	    document.querySelector('.exchanged-currency').innerHTML = "";
+	    const exchanged_amount = (currencyAmount / data.rates[dropdownInputSplit]).toFixed(2);
+	    exchangedCurrency.insertAdjacentHTML('afterbegin', `<br><div class="currency-span"><h3>You'll get <b>${exchanged_amount} Euros</b> back<h3>!</div></br>`);
+	  })
+	})
+}
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,3 +4,9 @@ require("channels")
 require("ajax-currency-manipulation/ajax")
 
 import "bootstrap";
+import { initMapbox } from '../plugins/init_mapbox';
+
+initMapbox();
+// document.addEventListener('turbolinks:load', () => {
+ 
+// })
