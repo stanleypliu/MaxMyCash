c1602199a347fbd980353b28115518f79a9ce8d5
Maxime Seiter <maxycle@hotmail.com>
2020-03-09 12:10:54 +0000

    popup window added

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -6,11 +6,11 @@ const initMapbox = () => {
   const fitMapToMarkers = (map, markers) => {
     const bounds = new mapboxgl.LngLatBounds();
     markers.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
-    map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 5 });
+    map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 5 }); 
   };
 
 
-  if (mapElement) { // only build a map if there's a div#map to inject into
+  if (mapElement) { 
     mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
     const map = new mapboxgl.Map({
       container: 'map',
@@ -18,24 +18,13 @@ const initMapbox = () => {
     });
     const markers = JSON.parse(mapElement.dataset.markers);
   	markers.forEach((marker) => {
+      const popup = new mapboxgl.Popup().setHTML(marker.infoWindow);
     	new mapboxgl.Marker()
       		.setLngLat([ marker.lng, marker.lat ])
+          .setPopup(popup) 
       		.addTo(map);
   	});
   	fitMapToMarkers(map, markers);
   }
 };
-// if (mapElement) {
-//   const markers = JSON.parse(mapElement.dataset.markers);
-//   markers.forEach((marker) => {
-//     new mapboxgl.Marker()
-//       .setLngLat([ marker.lng, marker.lat ])
-//       .addTo(map);
-//   });
-// }
-
-// if (mapElement) {
-//   // [ ... ]
-//   fitMapToMarkers(map, markers);
-// }
 export { initMapbox };
