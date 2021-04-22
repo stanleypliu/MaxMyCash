6489464b85d7d0bfa03e82bec07399dda2844dd8
maxime seiter <maxycle@hotmail.com>
2021-04-22 17:58:50 +0100

    init_mapbox changes accepted

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -1,31 +1,3 @@
-<<<<<<< HEAD
-import 'mapbox-gl/dist/mapbox-gl.css';
-import mapboxgl from 'mapbox-gl';
-
-const buildMap = (mapElement) => {
-  mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
-  return new mapboxgl.Map({
-    container: 'map',
-    style: 'mapbox://styles/mapbox/streets-v10'
-  });
-};
-
-const addMarkersToMap = (map, markers) => {
-  markers.forEach((marker) => {
-      const element = document.createElement('div');
-      element.className = 'marker';
-      element.style.backgroundImage = `url('${marker.image_url}')`;
-      element.style.backgroundSize = 'contain';
-      element.style.width = '25px';
-      element.style.height = '25px';
-    const popup = new mapboxgl.Popup().setHTML(marker.infoWindow);
-    new mapboxgl.Marker(element)
-      .setLngLat([ marker.lng, marker.lat ])
-      .setPopup(popup) 
-      .addTo(map);
-  });
-};
-=======
 import mapboxgl from 'mapbox-gl'
 
 const initMapbox = () => {
@@ -36,17 +8,9 @@ const initMapbox = () => {
     markers.forEach(marker => bounds.extend([marker.lng, marker.lat]))
     map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 5 })
   }
->>>>>>> bfdb917a09bcc155db7903892c6d688cb838892c
 
 
   if (mapElement) {
-<<<<<<< HEAD
-    const map = buildMap(mapElement);
-    const markers = JSON.parse(mapElement.dataset.markers);
-    console.log(markers);
-    addMarkersToMap(map, markers);
-    fitMapToMarkers(map, markers);
-=======
     mapboxgl.accessToken = mapElement.dataset.mapboxApiKey
     const map = new mapboxgl.Map({
       container: 'map',
@@ -65,7 +29,6 @@ const initMapbox = () => {
     })
     fitMapToMarkers(map, markers)
     map.addControl(new mapboxgl.NavigationControl())
->>>>>>> bfdb917a09bcc155db7903892c6d688cb838892c
   }
 }
 
