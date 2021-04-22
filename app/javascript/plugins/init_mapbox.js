8e7a5e6a2fd28cdf25e9e7ae431bcdb92a59db08
maxime seiter <maxycle@hotmail.com>
2021-04-22 15:52:30 +0100

    infinite scroll added

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import 'mapbox-gl/dist/mapbox-gl.css';
 import mapboxgl from 'mapbox-gl';
 
@@ -24,22 +25,48 @@ const addMarkersToMap = (map, markers) => {
       .addTo(map);
   });
 };
-
-const fitMapToMarkers = (map, markers) => {
-  const bounds = new mapboxgl.LngLatBounds();
-  markers.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
-  map.fitBounds(bounds, { padding: 70, maxZoom: 15 });
-};
+=======
+import mapboxgl from 'mapbox-gl'
 
 const initMapbox = () => {
-  const mapElement = document.getElementById('map');
+  const mapElement = document.getElementById('map')
+
+  const fitMapToMarkers = (map, markers) => {
+    const bounds = new mapboxgl.LngLatBounds()
+    markers.forEach(marker => bounds.extend([marker.lng, marker.lat]))
+    map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 5 })
+  }
+>>>>>>> bfdb917a09bcc155db7903892c6d688cb838892c
+
+
   if (mapElement) {
+<<<<<<< HEAD
     const map = buildMap(mapElement);
     const markers = JSON.parse(mapElement.dataset.markers);
     console.log(markers);
     addMarkersToMap(map, markers);
     fitMapToMarkers(map, markers);
+=======
+    mapboxgl.accessToken = mapElement.dataset.mapboxApiKey
+    const map = new mapboxgl.Map({
+      container: 'map',
+      style: 'mapbox://styles/mapbox/streets-v10'
+    })
+    const markers = JSON.parse(mapElement.dataset.markers)
+    markers.forEach((marker) => {
+      const popup = new mapboxgl.Popup().setHTML(marker.infoWindow)
+      new mapboxgl.Marker({
+        color: "#AFFFFA",
+        draggable: true
+      })
+        .setLngLat([marker.lng, marker.lat])
+        .setPopup(popup)
+        .addTo(map)
+    })
+    fitMapToMarkers(map, markers)
+    map.addControl(new mapboxgl.NavigationControl())
+>>>>>>> bfdb917a09bcc155db7903892c6d688cb838892c
   }
-};
+}
 
-export { initMapbox };
+export { initMapbox }
